c3a73ee454bf63d56d4cd2fbf4d87d164d231848
rhtmdals04 <devrhtmdals04@gmail.com>
2026-01-15 16:35:12 +0900

    1/15

---
+++ DSA/week04/19.remove-nth-node-from-end-of-list.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=19 lang=python
+#
+# [19] Remove Nth Node From End of List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        dummy = ListNode(0, head)
+        left, right = dummy, head
+
+        while n < 0:
+            right = right.next
+            n -= 1
+
+        while right:
+            left = left.next
+            right = right.next
+        
+        left.next = left.next.next
+        return dummy.next
+        
+# @lc code=end
+
--- DSA/week04/21.merge-two-sorted-lists.py
+++ DSA/week04/21.merge-two-sorted-lists.py
@@ -12,11 +12,32 @@
 #         self.next = next
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
+        dummy = node = ListNode()
+        dummy.next = node
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                node.next = list1
+                list1 = list1.next
+            else:
+                node.next = list2
+                list2 = list2.next
+            node = node.next
         
+        node.next = list1 or list2
+        return dummy.next
+    
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
+        if list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
 # @lc code=end
 
