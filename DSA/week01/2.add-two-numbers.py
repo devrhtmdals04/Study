fba571eb66187902066aa88dcfb3c5c362ac1647
rhtmdals04 <devrhtmdals04@gmail.com>
2026-01-19 14:25:14 +0900

    1/19

--- DSA/week01/2.add-two-numbers.py
+++
@@ -1,8 +0,0 @@
-class ListNode:
-    def __init__(self, val = 0, next = None):
-        self.val = val
-        self.next = next
-
-class Solution(object):
-    def add_two_numbers(self, l1: Optional[ListNode], l2: Optional[ListNode]):
-        pass
---
+++ DSA/week05/287.find-the-duplicate-number.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=287 lang=python
+#
+# [287] Find the Duplicate Number
+#
+
+# @lc code=start
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+# @lc code=end
+
