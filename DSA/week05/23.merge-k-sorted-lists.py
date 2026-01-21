9d357ad2350615f6875aa5c10dc13d4b6c9fe40f
rhtmdals04 <devrhtmdals04@gmail.com>
2026-01-21 15:55:24 +0900

    1/21

---
+++ DSA/week05/23.merge-k-sorted-lists.py
@@ -0,0 +1,44 @@
+#
+# @lc app=leetcode id=23 lang=python
+#
+# [23] Merge k Sorted Lists
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        if not lists:
+            return None
+        
+        while len(lists) > 1:
+            merged = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i+1] if (i+1) < len(lists) else None
+                merged.append(self.mergeTwoLists(l1,l2))
+            lists = merged
+        return lists[0]
+    
+    def mergeTwoLists(self, l1, l2):
+        dummy = ListNode()
+        curr = dummy
+
+        while l1 and l2:
+            if l1.val > l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        
+        curr.next = l1 if l1 else l2
+        return dummy.next
+# @lc code=end
+
