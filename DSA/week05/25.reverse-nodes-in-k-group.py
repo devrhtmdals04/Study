7e80723a52f090e219077e313ad1817fe428e183
rhtmdals04 <devrhtmdals04@gmail.com>
2026-01-23 10:05:45 +0900

    1/22

---
+++ DSA/week05/25.reverse-nodes-in-k-group.py
@@ -0,0 +1,44 @@
+#
+# @lc app=leetcode id=25 lang=python
+#
+# [25] Reverse Nodes in k-Group
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def reverseKGroup(self, head, k):
+        dummy = ListNode(0, head)
+        groupPrev = dummy
+
+        while True:
+            kth = self.findKth(groupPrev, k)
+            if not kth:
+                break
+            groupNext = kth.next
+
+            prev, curr = kth.next, groupPrev.next
+            while curr != groupNext:
+                tmp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = tmp
+
+            tmp = groupPrev.next
+            groupPrev.next = kth
+            groupPrev = tmp
+        return dummy.next
+    
+    def findKth(self, curr, k):
+        while curr and k > 0:
+            curr = curr.next
+            k -= 1
+        return curr
+        
+# @lc code=end
+
