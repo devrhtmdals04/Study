540b221ab6bac3990930b257f8aa04d1b474bcd5
rhtmdals04 <devrhtmdals04@gmail.com>
2026-02-04 09:42:40 +0900

    2/4

---
+++ DSA/week07/105.construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=105 lang=python
+#
+# [105] Construct Binary Tree from Preorder and Inorder Traversal
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: Optional[TreeNode]
+        """
+        
+# @lc code=end
+
--- DSA/week07/230.kth-smallest-element-in-a-bst.py
+++ DSA/week07/230.kth-smallest-element-in-a-bst.py
@@ -13,11 +13,43 @@
 #         self.right = right
 class Solution(object):
     def kthSmallest(self, root, k):
-        """
-        :type root: Optional[TreeNode]
-        :type k: int
-        :rtype: int
-        """
-        
+        cnt = k
+        res = root.val
+        def dfs(node):
+            nonlocal cnt, res
+            if not node:
+                return None
+            
+            dfs(node.left)
+            cnt -= 1
+            if cnt == 0:
+                res = node.val
+                return
+            dfs(node.right)
+        dfs(root)
+        return res
+
+class Solution(object):
+    def kthSmallest(self, root, k):
+        curr = root
+        while curr:
+            if not curr.left:
+                k -= 1
+                if k == 0:
+                    return curr.val
+                curr = curr.right
+            else:
+                pred = curr.left
+                while pred.right and pred.right != curr:
+                    pred = pred.right
+                if not pred.right:
+                    pred.right = curr
+                    curr = curr.left
+                else:
+                    pred.right = None
+                    k -= 1
+                    if k == 0:
+                        return curr.val
+                    curr = curr.right
 # @lc code=end
 
