4fb1a69b99e75943a9c84969e44ab53886d6d787
rhtmdals04 <devrhtmdals04@gmail.com>
2026-02-04 11:27:58 +0900

    2/4

--- DSA/week07/105.construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ DSA/week07/105.construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -4,20 +4,61 @@
 # [105] Construct Binary Tree from Preorder and Inorder Traversal
 #
 
-# @lc code=start
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution(object):
     def buildTree(self, preorder, inorder):
-        """
-        :type preorder: List[int]
-        :type inorder: List[int]
-        :rtype: Optional[TreeNode]
-        """
+        root = TreeNode(preorder[0], None, None)
+        i = inorder.index(preorder[0])
+
+        root.left = self.buildTree(preorder[1:i+1], inorder[:i])
+        root.right = self.buildTree(preorder[i+1:], inorder[i+1:])
+
+        return root
+
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        preIdx = inIdx = 0
+        def dfs(target):
+            nonlocal preIdx, inIdx
+            if preIdx >= len(preorder):
+                return None
+            if inorder[inIdx] == target:
+                inIdx += 1
+                return None
+            
+            root = TreeNode(preorder[preIdx])
+            preIdx += 1
+            root.left = dfs(root.val)
+            root.right = dfs(target)
+            return root
+        return dfs(float('inf'))
+
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        head = TreeNode(None)
+        curr = head
+        i, j, n = 0, 0, len(preorder)
+
+        while i < n and j < n:
+            curr.right = TreeNode(preorder[i], right = curr.right)
+            curr = curr.right
+            i += 1
+            while i < n and curr.val != inorder[j]:
+                curr.left = TreeNode(preorder[i], right = curr)
+                curr = curr.left
+                i += 1
+            j += 1
+            while curr.right and j < n and curr.right.val == inorder[j]:
+                prev = curr.right
+                curr.right = None
+                curr = prev
+                j += 1
+        return head.right
         
 # @lc code=end
 
