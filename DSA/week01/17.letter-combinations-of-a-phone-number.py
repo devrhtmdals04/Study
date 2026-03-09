9c9c99b6d1d18f18627766ccc80f1486d88b60d4
rhtmdals04 <devrhtmdals04@gmail.com>
2026-03-09 09:58:11 +0900

    3/9

---
+++ DSA/week01/17.letter-combinations-of-a-phone-number.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode id=17 lang=python
+#
+# [17] Letter Combinations of a Phone Number
+#
+
+# @lc code=start
+class Solution(object):
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "qprs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        def backtrack(i, cur):
+            if len(cur) == len(digits):
+                res.append(cur)
+                return
+            for c in digitToChar[digits[i]]:
+                backtrack(i + 1, cur + c)
+            
+        if digits:
+            backtrack(0, "")
+        return res
+
+
+        
+# @lc code=end
+
--- DSA/week10/131.palindrome-partitioning.py
+++ DSA/week10/131.palindrome-partitioning.py
@@ -7,10 +7,27 @@
 # @lc code=start
 class Solution(object):
     def partition(self, s):
-        """
-        :type s: str
-        :rtype: List[List[str]]
-        """
-        
+        res = []
+        part = []
+
+        def dfs(i):
+            if i >= len(s):
+                res.append(part.copy())
+                return
+            
+            for j in range(i, len(s)):
+                if self.isPali(s, i, j):
+                    part.append(s[i:j+1])
+                    dfs(j+1)
+                    part.pop()
+        dfs(0)
+        return res
+
+    def isPali(self, s, l, r):
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l, r = l + 1, r - 1
+        return True
 # @lc code=end
 
