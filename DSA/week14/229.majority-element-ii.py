b6d493d68f1e5945e84856a737ae65f4f2f380e2
rhtmdals04 <devrhtmdals04@gmail.com>
2026-04-07 10:20:54 +0900

    4/7

---
+++ DSA/week14/229.majority-element-ii.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=229 lang=python
+#
+# [229] Majority Element II
+#
+
+# @lc code=start
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+# @lc code=end
+
